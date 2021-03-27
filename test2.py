1b21b22952e1ec58976e29e73c523a0928b4163e
Anden Acitelli <a.acitelli@gmail.com>
2021-03-27 19:38:02 -0400

    Progress Bar; formatter;

--- test2.py
+++ test2.py
@@ -1,21 +1,24 @@
-# -*- coding: utf-8 -*-
 """
 Created on Wed Feb  3 14:37:32 2021
 
 @author: harsh
-"""
-"""
-example showing how to use pyPowderBedFusion for generating scan vector
+
+Example showing how to use pyPowderBedFusion for generating scan vector
 """
 
-import PowderBedFusion
-from PowderBedFusion import hatching as hatching
+# Standard Library Imports
+
+# Third-Party Imports
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+# Local Imports
+import PowderBedFusion
+from PowderBedFusion import hatching
 
 Part = PowderBedFusion.Part('Parameter_quality_nut_2')
 Part.setGeometry('Parameter_quality_nut_2.stl')
-
 Part.origin = [0.0, 0.0, 0.0]
 Part.rotation = np.array([0, 0, 90])
 Part.dropToPlatform()
@@ -26,68 +29,65 @@ myHatcher.islandWidth = 3.0
 myHatcher.stripeWidth = 5.0
 
 # Set the base hatching parameters which are generated within Hatcher
-myHatcher.hatchAngle = 45 # [°] The angle used for the islands
-myHatcher.volumeOffsetHatch = 0.08 # [mm] Offset between internal and external boundary
-myHatcher.spotCompensation = 0.06 # [mm] Additional offset to account for laser spot size
+myHatcher.hatchAngle = 45  # [°] The angle used for the islands
+# [mm] Offset between internal and external boundary
+myHatcher.volumeOffsetHatch = 0.08
+# [mm] Additional offset to account for laser spot size
+myHatcher.spotCompensation = 0.06
 myHatcher.numInnerContours = 2
 myHatcher.numOuterContours = 1
 myHatcher.hatchSortMethod = hatching.AlternateSort()
 
 # Set the layer thickness
-layerThickness = 1 # [mm]
+LAYER_THICKNESS = 1  # [mm]
 
-#Perform the hatching operations
-print('Hatching Started')
-
+# Perform the hatching operations
 layers = []
 
+print("Processing...")
+t = tqdm(np.arange(0, Part.boundingBox[5], LAYER_THICKNESS))
+for z in t:
 
-for z in np.arange(0, Part.boundingBox[5], layerThickness):
+    geom_slice = Part.getVectorSlice(z)  # Slice layer
+    layer = myHatcher.hatch(geom_slice)  # Hatch layer
 
-  print('Layer # ' + str(z) + ' Started')
+    # The layer height is set in integer increment of microns to ensure no rounding error during manufacturing
+    layer.z = int(z*1000)
+    for geometry in layer.geometry:
+        geometry.mid = 1
+        geometry.bid = 1
+    layers.append(layer)
 
-  # Slice the boundary
-  geomSlice = Part.getVectorSlice(z) 
+    # Explicitly refresh progress bar
+    t.refresh()
 
-  # Hatch the boundary using myHatcher
-  layer = myHatcher.hatch(geomSlice) 
-  
-  # The layer height is set in integer increment of microns to ensure no rounding error during manufacturing
-  layer.z = int(z*1000)
-  
-  layers.append(layer)
-  print('Layer # ' + str(z) + ' Complete\n')
-    
-# we have to assign a model and build style id to the layer geometry
-for layer in layers:
-  for layerGeom in layer.geometry:
-    layerGeom.mid = 1
-    layerGeom.bid = 1
-    
 bstyle = PowderBedFusion.genLayer.BuildStyle()
 bstyle.bid = 1
-bstyle.laserSpeed = 200.0 # [mm/s]
-bstyle.laserPower = 200 # [W]#
-bstyle.pointDistance = 60 # (60 microns)
-bstyle.pointExposureTime = 30 #(30 micro seconds)
-    
+bstyle.laserSpeed = 200.0  # [mm/s]
+bstyle.laserPower = 200  # [W]#
+bstyle.pointDistance = 60  # (60 microns)
+bstyle.pointExposureTime = 30  # (30 micro seconds)
 
 model = PowderBedFusion.genLayer.Model()
 model.mid = 1
 model.buildStyles.append(bstyle)
 resolution = 0.2
 
-# Plot the layer geometries using matplotlib
+# Plot the results
 PowderBedFusion.outputtools.plotLayers(layers[0:len(layers)])
 
+'''
 # Plot the corresponding layers
-i=0
+i = 0
 for layer in layers:
-    name = 'Layer Files for Parameter quality nut 2/Layer' + str(i) + '.pdf'
+    PowderBedFusion.visualize.plot(
+        layers[i], plot3D=False, plotOrderLine=True, plotArrows=True, handle=(fig, ax))
+    name = 'LayerFiles/Layer' + str(i) + '.pdf'
+    print("name: {}".format(name))
     fig, ax = plt.subplots(figsize=(20, 10))
-    PowderBedFusion.outputtools.plot(layers[i], plot3D=False, plotOrderLine=True, plotArrows=True, handle=(fig,ax))
+    
     fig.savefig(name, bbox_inches='tight')
-    print(name)
     plt.cla()
-    plt.close(fig) 
+    plt.close(fig)
     i = i+1
+'''
