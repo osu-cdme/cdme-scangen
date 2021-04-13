2259f9d740c834b427f54dfd454e4dd8a7786efb
Anden Acitelli <a.acitelli@gmail.com>
2021-04-13 00:18:35 -0400

    Vector splitting theoretically working

--- standardization/standardization.py
+++ standardization/standardization.py
@@ -2,29 +2,42 @@
 Vector Standardization
 =====
 
-Provides
-  1. Splitting vectors above a certain length via `split_long_vectors()`
-  2. "Extending" vectors below a certain length by adding jump vectors via `extend_short_vectors()`
+Provides:
+1. Splitting vectors above a certain length via `split_long_vectors()`
+2. "Extending" vectors below a certain length by adding jump vectors via `extend_short_vectors()`
+
+Relies on the following third-party libraries:
+1. `numpy` for faster array operations
+2. `numba` for just-in-time compilation
 
 Notes:
-- Functions have docstrings autoconfigured to work with Sphinx. While that is not currently set up, you should be able to use it to automatically generate documentation formatted identically to https://pyslm.readthedocs.io/en/latest/index.html using the docstrings found here.
+- Functions have docstrings autoconfigured to work with Sphinx. That is not currently set up, but you should be able to set it up to automatically generate documentation formatted identically to https://pyslm.readthedocs.io/en/latest/index.html using the docstrings found here.
 """
 
-from defs import Vertex, Segment, BoundingBox
-import numpy as np
+# Standard Library Imports
 import math
+from copy import deepcopy
+
+# Third-Party Imports
+import numpy as np
 from nptyping import NDArray
+from numba import njit
+import timeit
+
+# Local Imports
+from defs import Vertex, Segment, BoundingBox
 
 
+@njit
 def split_long_vectors(segment_list: NDArray[Segment], cutoff: int) -> NDArray[Segment]:
     """Splits a given list of segments such that no post-split segment is longer than `cutoff`.
 
     :param segment_list: A list of segments that should be split.
-    :type segment_list: NDArray[Segment]
+    :type segment_list: NDArray[class:`Segment`]
     :param cutoff: The maximum length that a given vector can be.
     :type cutoff: int
     :return: Returns the vector list split by `cutoff` length.
-    :rtype: np.array[Segment]
+    :rtype: np.array[class:`Segment`]
     """
 
     # We know we will have at least as many segments as were passed in, so we preallocate to that
@@ -34,17 +47,18 @@ def split_long_vectors(segment_list: NDArray[Segment], cutoff: int) -> NDArray[S
     return output
 
 
+@njit
 def split_vector(segment: Segment, cutoff: int) -> NDArray[Segment]:
     """Splits a given segment into segments such that each individual segment is no longer than `cutoff`.
 
     Note: With `cutoff` = 1mm, will split a 5.14mm vector into 1-1-1-1-1-.14 rather than dividing it equally.
 
     :param segment: The segment to split.
-    :type segment: Segment
+    :type segment: class:`Segment`
     :param cutoff: The maximum length that subsegments should be.
     :type cutoff: int
     :return: Returns the given vector split by `cutoff` length.
-    :rtype: np.array[Segment]
+    :rtype: np.array[class:`Segment`]
     """
 
     # Segment is short enough to not need split
@@ -59,28 +73,37 @@ def split_vector(segment: Segment, cutoff: int) -> NDArray[Segment]:
         segment, cutoff * segments_away // segment.length)
     while math.dist(end_point, segment.v1) <= segment.length + .0000001:
         output.put(Segment(start_point, end_point))
+        segments_away += 1
+        start_point = deepcopy(end_point)
+        end_point = get_scaled_point(
+            segment, (cutoff * segments_away) // segment.length)
+    return output
 
 
+@njit
 def get_scaled_point(segment: Segment, fraction: float) -> Vertex:
     """Takes the provided segment and returns the subsegment starting at the provided fraction along the part.
 
     TODO: Make sure the documentation of this function is accurate. It's confusing at best.
 
-    :param segment: The segment we are extending.
-    :type segment: Segment
-    :param percent: The fraction along the overall segment that the end of this subsegment should be.
-    :type percent: float
+    :param segment: The starting vertex of the overall, full-length segment.
+    :type segment: class:`Segment`
+    :param fraction: The fraction along the overall segment that the end of this subsegment should be.
+    :type fraction: float
     :return: The subset of the given vector.
     :rtype: Vertex
     """
 
     v = Vertex()
-    if segment.v2.x > segment.v1.x:  # Calculate scaled x
-        v.x = segment.v1.x + fraction * (segment.v2.x - segment.v1.x)
-    else:
-        v.x = segment.v1.x - fraction * (segment.v1.x - segment.v2.x)
-    if segment.v2.y > segment.v1.y:  # Calculate scaled y
-        v.y = segment.v1.y + fraction * (segment.v2.y - segment.v1.y)
-    else:
-        v.y = segment.v1.y - fraction * (segment.v1.y - segment.v2.y)
+
+    # Scaled X
+    v.x = segment.v1.x + fraction * \
+        (segment.v2.x - segment.v1.x) if segment.v2.x > segment.v1.x else segment.v1.x - \
+        fraction * (segment.v1.x - segment.v2.x)
+
+    # Scaled Y
+    v.y = segment.v1.y + fraction * \
+        (segment.v2.y - segment.v1.y) if segment.v2.y > segment.v1.y else segment.v1.y - \
+        fraction * (segment.v1.y - segment.v2.y)
+
     return v
