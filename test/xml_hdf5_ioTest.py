914b7bcefc8d7b9f159c09700026c3eaf57c8916
John Cook <74627489+jjocook@users.noreply.github.com>
2021-10-29 11:27:45 -0400

    passed in parameters updated

--- test/xml_hdf5_ioTest.py
+++ test/xml_hdf5_ioTest.py
@@ -9,19 +9,9 @@ from src.output.xml_hdf5_io import XMLWriter,ConfigFile
 
 
 outputDir="C:/CDME/Code/xml"
-print_loader=ConfigFile("C:/CDME/Code/cdme-scangen/build_config.xls")
 
-xmlWriter = XMLWriter(outputDir,print_loader)
-xmlWriter.output_xml(print_loader.parts,print_loader.seg_style,ScanMode.ContourFirst)
+
+xmlWriter = XMLWriter(outputDir)
+xmlWriter.output_xml(layers,model)
 xmlWriter.output_zip()
 
-#def output_xml(self, layers_paths: List[List[np.ndarray]], layers_segstyles: List[str], scan_mode: ScanMode):
-#class ConfigFile():
-#    def __init__(self, file_path):
-#        self.config_path = file_path
-#        self.header = self.load_header()
-#        self.vel_profile = self.load_velocity_profile()
-#        self.seg_style = self.load_segment_style()
-#        self.regions, self.parts = self.load_parts()
-
-#killing off the ConfigFile class in move to IO 2.0
