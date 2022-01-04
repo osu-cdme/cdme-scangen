15dce2fc91da824b0475d72e417806dc4d15db4f
Anden Acitelli <a.acitelli@gmail.com>
2022-01-04 11:56:27 -0500

    Add special handling for segment/velocity stuff

--- load_parameters.py
+++ load_parameters.py
@@ -8,18 +8,35 @@ def parse_config(config):
     parse_config_data_types(config, schema) # Parse everything into its data type specified in the schema
     return config
 
+# Need special handling
+special_keys = set(["Hatch Default ID", "Contour Default ID", "Segment Styles", "Velocity Profiles"])
+
 def default_config():
     with open("schema.json", "r") as f:
         schema = json.load(f)
     config = {} 
+
+    # Need specially handled
+    config["Hatch Default ID"] = schema["Hatch Default ID"]
+    config["Contour Default ID"] = schema["Contour Default ID"]
+    config["Segment Styles"] = schema["Segment Styles"]
+    config["Velocity Profiles"] = schema["Velocity Profiles"]
+
+    # Can handle as a general case
     for category in schema: # Fill with default values according to the schema 
+        if category in special_keys:
+            continue
         for attribute in schema[category]: 
             config[attribute["name"]] = attribute["default"]
+
+    # Convert to correct data types, as by default they are stringified
     parse_config_data_types(config, schema)       
     return config
 
 def parse_config_data_types(config, schema):
     for category in schema:
+        if category in special_keys:
+            continue
         for attribute in schema[category]:
             config[attribute["name"]] = get_value_of_attribute(config[attribute["name"]], attribute["type"])
 
